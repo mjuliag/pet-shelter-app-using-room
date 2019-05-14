698ad2abe20ac69841565e7ac6e09504b0a1fe00
nekomaneki <mjuliagirona@gmail.com>
2019-05-13 21:35:47 -0300

    gradle issues

---
+++ app/src/main/java/com/example/android/pets/Entities/Pets.java
@@ -0,0 +1,9 @@
+package com.example.android.pets.Entities;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "pets")
+public class Pets {
+}
